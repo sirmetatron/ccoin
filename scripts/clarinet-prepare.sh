5be538e7d2dd83900b67b3fc816b611b628b3ec9
sirmetatron <104751839+sirmetatron@users.noreply.github.com>
2022-05-01 23:54:19 -0600

    Initialize fix/sip-010-names

---
+++ scripts/clarinet-prepare.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+contracts_dir_rel="${script_dir}/../contracts"
+contracts_dir=$(readlink -e "$contracts_dir_rel")
+contracts_clarinet_dir="${contracts_dir}/clarinet"
+contracts_test_addons_dir="${contracts_dir}/test_addons"
+
+sip10_address="SP1X6M947Z7E58CNE0H8YJVJTVKS9VW0PHD4Q0A5F.sip-010-trait.sip-010-trait"
+mock_sip10_address="ST1HTBVD3JG9C05J7HBJTHGR0GGW7KXW28M5JS8QE.sip-010-trait.sip-010-trait"
+
+rm -rf "${contracts_clarinet_dir}"
+mkdir -p "${contracts_clarinet_dir}"
+
+#
+shopt -s nullglob
+for f in "${contracts_dir}"/*.clar; do 
+  filePath=$(readlink -e "$f")
+  fileName=$(basename "${filePath}")
+  targetFilePath="${contracts_clarinet_dir}/$fileName"
+
+  cp "${filePath}" "${targetFilePath}"
+
+  sed -i -e "s|${sip10_address}|${mock_sip10_address}|g" "${targetFilePath}"
+
+  # # check if contract have test addon and adds its content to contract file
+  if [ -f "${contracts_test_addons_dir}/${fileName}" ]; then
+    cat "${contracts_test_addons_dir}/${fileName}" >> "${targetFilePath}"
+  fi;
+done;
+shopt -u nullglob
+
+if ! command -v clarinet &> /dev/null; then
+    echo "ERROR: clarinet could not be found"
+    exit 1
+fi
